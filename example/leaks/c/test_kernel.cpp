76bcdafbfbe3f69ca10f10782db4e0ce2ce9a0f2
mnhdxnh <947098055@qq.com>
2026-09-14 09:46:53 +0800

    [bugfix] 修复leaks案例kernel DataCopy越界导致的vector core exception(507035)

--- example/leaks/c/test_kernel.cpp
+++ example/leaks/c/test_kernel.cpp
@@ -3,20 +3,18 @@
 using namespace AscendC;
 
 constexpr int32_t BYTESIZE = 256;
-constexpr int32_t BYTESIZE_LARGE = 512;
 constexpr int32_t NUM_DATA = BYTESIZE / sizeof(half);
-constexpr int32_t NUM_DATA_LARGE = BYTESIZE_LARGE / sizeof(half);
 
 extern "C" __global__ __aicore__ void test_kernel(__gm__ uint8_t *gm)
 {
     TPipe pipe;
     TBuf<QuePosition::VECCALC> xlm;
     GlobalTensor<half> xGm;
-    pipe.InitBuffer(xlm, BYTESIZE_LARGE);
+    pipe.InitBuffer(xlm, BYTESIZE);
     LocalTensor<half> xLm = xlm.Get<half>();
     xGm.SetGlobalBuffer((__gm__ half *)gm, NUM_DATA);
-    DataCopy(xLm, xGm, NUM_DATA_LARGE);
-    DataCopy(xGm, xLm, NUM_DATA_LARGE);
+    DataCopy(xLm, xGm, NUM_DATA);
+    DataCopy(xGm, xLm, NUM_DATA);
 }
 
 extern "C" void test_kernel_do(uint32_t blockDim, void *l2ctrl, void *stream, uint8_t *gm)
